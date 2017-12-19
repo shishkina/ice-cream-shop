5e17306f1d532af8abc08b5e8aa0ddd0d7ad9e68
shishkina <shishkina.kate@gmail.com>
2017-12-19 12:06:35 -0500

    move the final app to a separate repo

--- icecream-app-final/db/migrations/migration-111320171200.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF not exists icecream (
-  id SERIAL PRIMARY KEY,
-  flavor VARCHAR(255),
-  description VARCHAR(255),
-  rating VARCHAR(6),
-  url VARCHAR(255),
-  brand VARCHAR(255)
-);
